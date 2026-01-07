ab161c149c914fa24321c23bbb09b0ec0c91b6e8
ob1 <ongjjbrandon@gmail.com>
2026-01-07 07:54:24 +0000

    edit the workspace path

--- ob1_setup_new_server.sh
+++ ob1_setup_new_server.sh
@@ -1,16 +1,24 @@
-mkdir -p /ob1_ws/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /ob1_ws/miniconda3/miniconda.sh
-bash /ob1_ws/miniconda3/miniconda.sh -b -u -p /ob1_ws/miniconda3
-rm /ob1_ws/miniconda3/miniconda.sh
+apt update
+apt install tmux -y
 
-source /ob1_ws/miniconda3/bin/activate
+mkdir -p /workspace/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /workspace/miniconda3/miniconda.sh
+bash /workspace/miniconda3/miniconda.sh -b -u -p /workspace/miniconda3
+rm /workspace/miniconda3/miniconda.sh
+
+source /workspace/miniconda3/bin/activate
 
 conda init --all
 
-conda config --add envs_dirs /ob1_ws/conda/envs
-conda config --add pkgs_dirs /ob1_ws/conda/pkgs
+conda config --add envs_dirs /workspace/conda/envs
+conda config --add pkgs_dirs /workspace/conda/pkgs
 
 conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
 conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 
 ./install_env.sh 3dgrut WITH_GCC11
+
+conda activate 3dgrut
+
+# if system uses GCC >12 then install gcc=11 gxx=11
+conda install -c conda-forge gcc=11 gxx=11
